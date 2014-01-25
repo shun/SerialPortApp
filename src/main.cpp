2deb7c50563fd7410a35ad860ff90e2960c06521
Shunsuke Kudo <skudo_xx@yahoo.co.jp>
2014-01-25 22:35:32 +0800

    implement qml

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
+#include <QDeclarativeView>
 #include <QApplication>
 #include "SerialPortClient.h"
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+
+	QDeclarativeView *view = new QDeclarativeView();
+	view->setSource(QUrl::fromLocalFile("./qml/main.qml"));
+	view->show();
 
     return app.exec();
 }
